ca8488222af57bbb3fe9f8569bb88915e6d8c034
TwoStoneSMP <svennijhuis1976@gmail.com>
2021-06-12 15:56:43 +0200

    send 13

--- commands/tosti.js
+++ commands/tosti.js
@@ -11,6 +11,7 @@ module.exports = {
             .setTitle(`Tosti`)
             .setColor("#006eff")
             .addField("Tosti's zijn altijd sad", "Net zoals @Sad Tosti#8649")
+            .setThumbnail('https://cdn.discordapp.com/emojis/851919025306468382.png?v=1')
             .setFooter("Copyright © | Forum voor Democratie 2021", "https://cdn.discordapp.com/attachments/807245844213530695/853254859268947968/ezgif-7-8d9d8c257f24.gif");
         message.channel.send(ledenEmbed);
     }
