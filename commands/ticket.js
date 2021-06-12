c9771bb2ab2f782f15086b088870528577a67384
TwoStoneSMP <svennijhuis1976@gmail.com>
2021-06-12 15:38:28 +0200

    Update ticket.js

--- commands/ticket.js
+++ commands/ticket.js
@@ -41,7 +41,7 @@ module.exports = {
 
       const TicketMessage = new Discord.MessageEmbed()
         .setTitle(`Hallo ${message.author.username},`)
-        .setDescription(`Uw support ticket is aangemaakt ${channel}`)
+        .setDescription(`Uw tockiet is aangemaakt ${channel}`)
         .setFooter("Copyright © | Forum voor Democratie 2021", "https://cdn.discordapp.com/attachments/807245844213530695/853254859268947968/ezgif-7-8d9d8c257f24.gif");
 
       message.channel
