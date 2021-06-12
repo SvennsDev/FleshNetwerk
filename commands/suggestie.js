33f1eaeef70ccff8e73f6a3f75809b199878d158
TwoStoneSMP <svennijhuis1976@gmail.com>
2021-06-12 16:04:22 +0200

    send 15

--- commands/suggestie.js
+++ commands/suggestie.js
@@ -12,7 +12,7 @@ module.exports = {
         .setTitle("Suggestie")
         .setColor("#007bff")
         .setDescription(messageArgs)
-        .addField(`Door:`, message.author.nickname, message.author.displayAvatarURL({ dynamic: true }))
+        .addField(`Door:`, message.author.username, message.author.displayAvatarURL({ dynamic: true }))
         .setTimestamp()
         .setFooter("Copyright © | Forum voor Democratie 2021", "https://cdn.discordapp.com/attachments/807245844213530695/853254859268947968/ezgif-7-8d9d8c257f24.gif");
 
