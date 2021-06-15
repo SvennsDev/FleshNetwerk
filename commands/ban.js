bd539cf835a548946d3137140b06b4dae293304e
TwoStoneSMP <svennijhuis1976@gmail.com>
2021-06-15 11:20:05 +0200

    d

--- commands/ban.js
+++ commands/ban.js
@@ -4,18 +4,19 @@ module.exports = {
     execute(message, args){
         const target = message.mentions.users.first();
         if(target){
-            var embedCreateTicket = new Discord.MessageEmbed()
-            .setTitle("User verbannen")
-            .setDescription(`**${memberTarget}**is verbannen!`)
-            .setTimestamp()
-            .setFooter("Copyright © | Forum voor Democratie 2021", "https://cdn.discordapp.com/attachments/807245844213530695/853254859268947968/ezgif-7-8d9d8c257f24.gif");    
             const memberTarget = message.guild.members.cache.get(target.id);
             memberTarget.ban();
-            var BanLog = message.member.guild.channels.cache.find(channel => channel.name === "🔒╹moderator-only");
-            BanLog.send(embedCreateTicket);
+            var embedCreateTicket = new Discord.MessageEmbed()
+                .setTitle("User verbannen")
+                .setDescription(`**${memberTarget}**is verbannen!`)
+                .setTimestamp()
+                .setFooter("Copyright © | Forum voor Democratie 2021", "https://cdn.discordapp.com/attachments/807245844213530695/853254859268947968/ezgif-7-8d9d8c257f24.gif");    
+          var ticketChannel = message.member.guild.channels.cache.find(channel => channel.name === "🔒╹moderator-only");
+          if (!ticketChannel) return message.reply("Kanaal bestaat niet");
+          ticketChannel.send(embedCreateTicket);
             message.channel.send(`${memberTarget} is gebanned`);
         }else{
-            message.channel.send(`Jij kan dit niet doen!`);
+            message.channel.send(`Geef een speler op!`);
         }
     }
-}
+} 
