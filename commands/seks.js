68024d64fed38d55488fe7792f87316e25a7508e
TwoStoneSMP <svennijhuis1976@gmail.com>
2021-06-24 19:28:00 +0200

    Update seks.js

--- commands/seks.js
+++ commands/seks.js
@@ -4,7 +4,7 @@ module.exports = {
     async execute(message, args, client, Discord, discord){
         var categoryID = "857671953347248138";
 
-        if (message.channel.parentID !== categoryID) return message.reply("Dit command kan alleen in #🔞╹nsfw") && message.delete();
+        if (message.channel.parentID !== categoryID) return message.reply("Dit command kan alleen in #🔞╹nsfw") && message.channel.bulkDelete(3);
 
         var ledenEmbed = new Discord.MessageEmbed()
             .setTitle(`Seks?`)
